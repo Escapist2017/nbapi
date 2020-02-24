1418af43535b7e7c566f0d39addaaf148bf7d194
escapist <15669909922@139.com>
2020-02-24 13:44:11 +0800

    update urls

--- NbApi/urls.py
+++ NbApi/urls.py
@@ -4,7 +4,7 @@ import xadmin
 from django.urls import path, re_path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
-from ecels.views import CellsLevelViewSet, CellsTypeViewSet, CellsDetailViewSet
+from ecels.views import CellsLevelViewSet, CellsTypeViewSet, CellsDetailViewSet, CellExportView
 from users.views import SmsCodeViewset, UserViewset
 from rest_framework.authtoken import views
 from rest_framework_jwt.views import obtain_jwt_token
@@ -26,6 +26,6 @@ urlpatterns = [
     # path('api-token-auth/', views.obtain_auth_token),
     # jwt认证接口
     path('login/', obtain_jwt_token),
-
     path('', include(router.urls)),
+    path('cellexport',CellExportView.as_view(), name='cellexport')
 ]
